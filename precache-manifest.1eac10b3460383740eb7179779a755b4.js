487b38345f83b68a2c9eeb8ed030d142b0868439
db <talktodonnybronson@gmail.com>
2019-05-15 19:19:39 +0200

    commiting first build

---
+++ precache-manifest.1eac10b3460383740eb7179779a755b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c0c2940a5da05aeca17",
+    "url": "/static/css/main.f2e32ee0.chunk.css"
+  },
+  {
+    "revision": "7c0c2940a5da05aeca17",
+    "url": "/static/js/main.7c0c2940.chunk.js"
+  },
+  {
+    "revision": "ac7cf29df971205d0815",
+    "url": "/static/js/1.ac7cf29d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7004482018d309d39261414a6d31ae92",
+    "url": "/index.html"
+  }
+];
